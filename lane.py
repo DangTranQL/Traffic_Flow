fb4aa4473838168e7b198e60db97be228bcf2716
DangTran <72413423+DangTranQL@users.noreply.github.com>
2023-06-22 17:26:56 -0400

    Add files via upload

---
+++ lane.py
@@ -0,0 +1,86 @@
+import pygame
+import sys
+import time
+import math
+import random
+from utils import *
+from car import *
+from path_tracking import *
+
+pygame.font.init()
+
+street = pygame.image.load("imgs/street.png")
+width, height = street.get_width(), street.get_height()
+gameDisplay = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Simulation")
+font = pygame.font.SysFont("comicsans", 44)
+
+
+class GameInfo:
+    def __init__(self):
+        self.started = False
+        self.start_time = 0
+
+    def reset(self):
+        self.started = False
+        self.start_time = 0
+
+    def game_finished(self):
+        return False
+
+    def start_run(self):
+        self.started = True
+        self.start_time = time.time()
+
+def draw(display, images, car, game_info):
+    for img, pos in images:
+        display.blit(img, pos)
+
+    car.draw(display)
+    pygame.display.update()
+    
+fps = 60
+clock = pygame.time.Clock()
+run = True
+
+images = [(street, (0,0))]
+game_info = GameInfo()
+path = State('straight')[2]
+car = Car(1,4)
+
+while run:
+    clock.tick(fps)
+    
+    draw(gameDisplay, images, car, game_info)
+    
+    while not game_info.started:
+        blit_text_center(gameDisplay, font, f"Press any key to start")
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                # pygame.quit()
+                break
+
+            if event.type == pygame.KEYDOWN:
+                game_info.start_run()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    car.move()
+    
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         run = False
+    #         pygame.quit()
+    #         break
+    #     if event.type == pygame.MOUSEBUTTONDOWN:
+    #         pos = pygame.mouse.get_pos()
+    #         car.path.append(pos)
+# print(car.path)
+
+pygame.quit()
